7867d06b1c49f7e74cb451032b067e537248e25f
mohammed sebti <mohammed.sebti@agqlabs.com>
2026-04-12 18:10:11 +0200

    first commiddtfdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -212,7 +212,7 @@ const App = () => {
         window.location.hostname === "127.0.0.1"
           ? "http://127.0.0.1:8000"
           : "https://mihorariosalat.pythonanywhere.com";
-
+      
       try {
         const res = await axios.get(
           `${API_BASE_URL}/api/schedule?city=${city}&country=${country}`
